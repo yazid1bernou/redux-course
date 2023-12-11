db2b738170a8354e51ee3a67fa42ac45206d2d51
unknown <yazid1bernou@gmail.com>
2023-12-11 09:33:02 +0000

    create reducer function

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ console.log("Hello in redux");
 const BY_CAKE = "BY_CAKE";
 
 {
-  type :  BY_CAKE ,
-  info : "first action in redux"
+   type :  BY_CAKE ,
+  
+}
+
+const byCake =  () => {
+    return(
+       {
+        type :  BY_CAKE ,
+        info : "the first action redux"
+       
+       })
+
+}
+
+const initialState =  {
+    numbersOfCake : 10 
+}
+
+const reducer = ( state = initialState , action ) => {
+     switch(action.type) {
+        case "BUY_CAKE" :
+            return {
+                ...state ,
+                numbersOfCake : state.numbersOfCake - 1 
+            }
+     }
 }

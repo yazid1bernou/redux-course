ba431416cfb0854088b85f8a79644b8c4fa2214b
unknown <yazid1bernou@gmail.com>
2023-12-11 13:36:30 +0000

    continued to create reducer and dispatch redux

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-console.log("Hello in redux");
+const redux = require('redux'); 
+const createStore = redux.createStore;
 
-const BY_CAKE = "BY_CAKE";
+console.log("Hello in redux"); 
 
-{
-   type :  BY_CAKE ,
-  
-}
+const BY_CAKE = "BY_CAKE"; 
 
-const byCake =  () => {
-    return(
-       {
-        type :  BY_CAKE ,
-        info : "the first action redux"
-       
-       })
-
-}
-
-const initialState =  {
-    numbersOfCake : 10 
-}
-
-const reducer = ( state = initialState , action ) => {
-     switch(action.type) {
-        case "BUY_CAKE" :
-            return {
-                ...state ,
-                numbersOfCake : state.numbersOfCake - 1 
-            }
-     }
-}
+function buyCake() {
+     return (
+        { 
+            type: BY_CAKE, 
+            info: "the first action redux" 
+    }) }
+ const initialState = {
+     numbersOfCake: 10, 
+     numberIccCream: 15
+    }
+const reducer =(state = initialState, action) => {
+         switch (action.type) {
+             case BY_CAKE:
+                 return {
+                     ...state,
+                      numbersOfCake: state.numbersOfCake - 1
+                     }; 
+             default: 
+                return state; }
+            }; 
+const store = createStore(reducer);
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() => { console.log("Update State", store.getState()); });
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
